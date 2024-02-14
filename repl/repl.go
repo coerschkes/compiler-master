1829a03684205b42f7ce781f63589c12c1ea599b
Christian Oerschkes <christian.oerschkes@hotmail.de>
2024-02-14 11:13:15 +0100

    feat: Extend repl to use the parser.

--- repl/repl.go
+++ repl/repl.go
@@ -3,7 +3,7 @@ package repl
 import (
 	"bufio"
 	"compiler/lexer"
-	"compiler/token"
+	"compiler/parser"
 	"fmt"
 	"io"
 )
@@ -21,9 +21,21 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			_, _ = fmt.Fprintf(out, "%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		_, _ = io.WriteString(out, program.String())
+		_, _ = io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		_, _ = fmt.Fprintf(out, "\t%s\n", msg)
 	}
 }
