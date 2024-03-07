9c21437d5677fe382911203f38989654cfbbe8e7
Christian Oerschkes <christian.oerschkes@hotmail.de>
2024-03-07 16:03:20 +0100

    feat: Implement a very basic compiler for compiling and evaluating integer literal expressions to bytecode.

---
+++ compiler/compiler.go
@@ -0,0 +1,80 @@
+package compiler
+
+import (
+	"compiler/ast"
+	"compiler/code"
+	"compiler/object"
+)
+
+type Compiler struct {
+	instruction code.Instructions
+	constants   []object.Object
+}
+
+type Bytecode struct {
+	Instructions code.Instructions
+	Constants    []object.Object
+}
+
+func New() *Compiler {
+	return &Compiler{
+		instruction: code.Instructions{},
+		constants:   []object.Object{},
+	}
+}
+
+func (c *Compiler) Compile(node ast.Node) error {
+	switch node := node.(type) {
+	case *ast.Program:
+		for _, s := range node.Statements {
+			err := c.Compile(s)
+			if err != nil {
+				return err
+			}
+		}
+	case *ast.ExpressionStatement:
+		err := c.Compile(node.Expression)
+		if err != nil {
+			return err
+		}
+	case *ast.InfixExpression:
+		err := c.Compile(node.Left)
+		if err != nil {
+			return err
+
+		}
+
+		err = c.Compile(node.Right)
+		if err != nil {
+			return err
+		}
+	case *ast.IntegerLiteral:
+		integer := &object.Integer{Value: node.Value}
+		c.emit(code.OpConstant, c.addConstant(integer))
+	}
+	return nil
+}
+
+func (c *Compiler) Bytecode() *Bytecode {
+	return &Bytecode{
+		Instructions: c.instruction,
+		Constants:    c.constants,
+	}
+}
+
+func (c *Compiler) addConstant(obj object.Object) int {
+	c.constants = append(c.constants, obj)
+	return len(c.constants) - 1
+}
+
+func (c *Compiler) emit(op code.Opcode, operands ...int) int {
+	ins := code.Make(op, operands...)
+	pos := c.addInstruction(ins)
+	return pos
+}
+
+func (c *Compiler) addInstruction(ins []byte) int {
+	posNewInstruction := len(c.instruction)
+	c.instruction = append(c.instruction, ins...)
+	return posNewInstruction
+}
