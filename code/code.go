13213d0098ea6eb2cbbd86e313963aaf1becdff6
Christian Oerschkes <christian.oerschkes@hotmail.de>
2024-03-07 21:19:29 +0100

    feat: Add support for subtract, multiply and divide operations.

--- code/code.go
+++ code/code.go
@@ -10,6 +10,9 @@ const (
 	OpConstant Opcode = iota
 	OpAdd
 	OpPop
+	OpSub
+	OpMul
+	OpDiv
 )
 
 type Opcode byte
@@ -23,6 +26,9 @@ var definitions = map[Opcode]*Definition{
 	OpConstant: {"OpConstant", []int{2}},
 	OpAdd:      {"OpAdd", []int{}},
 	OpPop:      {"OpPop", []int{}},
+	OpSub:      {"OpSub", []int{}},
+	OpMul:      {"OpMul", []int{}},
+	OpDiv:      {"OpDiv", []int{}},
 }
 
 type Instructions []byte
