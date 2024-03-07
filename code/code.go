00ded605d7550368660329d5a5b6fdfd1575515d
Christian Oerschkes <christian.oerschkes@hotmail.de>
2024-03-07 17:08:39 +0100

    feat: Add OpCode "OpAdd".

--- code/code.go
+++ code/code.go
@@ -8,7 +8,20 @@ import (
 
 const (
 	OpConstant Opcode = iota
+	OpAdd
 )
+
+type Opcode byte
+
+type Definition struct {
+	Name          string
+	OperandWidths []int
+}
+
+var definitions = map[Opcode]*Definition{
+	OpConstant: {"OpConstant", []int{2}},
+	OpAdd:      {"OpAdd", []int{}},
+}
 
 type Instructions []byte
 
@@ -24,35 +37,26 @@ func (ins Instructions) String() string {
 		}
 
 		operands, read := ReadOperands(def, ins[i+1:])
-		fmt.Fprintf(&out, "%04d %s\n", i, formatInstruction(def, operands))
+		fmt.Fprintf(&out, "%04d %s\n", i, fmtInstruction(def, operands))
 
 		i += 1 + read
 	}
 	return out.String()
 }
 
-func formatInstruction(def *Definition, operands []int) string {
+func fmtInstruction(def *Definition, operands []int) string {
 	operandCount := len(def.OperandWidths)
 	if len(operands) != operandCount {
 		return fmt.Sprintf("ERROR: operand len %d does not match defined %d\n", len(operands), operandCount)
 	}
 
 	switch operandCount {
+	case 0:
+		return def.Name
 	case 1:
 		return fmt.Sprintf("%s %d", def.Name, operands[0])
 	}
 	return fmt.Sprintf("ERROR: unhandled operandCount for %s\n", def.Name)
-}
-
-type Opcode byte
-
-type Definition struct {
-	Name          string
-	OperandWidths []int
-}
-
-var definitions = map[Opcode]*Definition{
-	OpConstant: {"OpConstant", []int{2}},
 }
 
 func Lookup(op byte) (*Definition, error) {
