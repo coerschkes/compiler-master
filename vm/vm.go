513f892b91f7234dcee7e53ccfac710a4c095a6d
Christian Oerschkes <christian.oerschkes@hotmail.de>
2024-03-19 21:52:46 +0100

    feat: Handle null when used with bang operator.

--- vm/vm.go
+++ vm/vm.go
@@ -102,6 +102,8 @@ func isTruthy(obj object.Object) bool {
 	switch obj := obj.(type) {
 	case *object.Boolean:
 		return obj.Value
+	case *object.Null:
+		return false
 	default:
 		return true
 	}
@@ -202,6 +204,8 @@ func (vm *VM) executeBangOperator() error {
 	case True:
 		return vm.push(False)
 	case False:
+		return vm.push(True)
+	case Null:
 		return vm.push(True)
 	default:
 		return vm.push(False)
